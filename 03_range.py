60353222461a6c41d42f6e3b3a78388821132368
Gabriel Rodrigues Bolonha Santos <gabriel-rodriguesbs@hotmail.com>
2023-02-23 19:09:09 -0300

    feat: create range examples
    create range examples with one parameter, 2 parameters and 3 parameters

---
+++ 03_range.py
@@ -0,0 +1,26 @@
+#range gera uma faixa de números
+for x in range(10):
+    print(x)
+
+print("-"*80)
+
+#range com 2 parametros gera uma lista que vai do primeiro parametro ate o segundo
+#[1ºParametro, 2ºParametro[
+for y in range(5,12):
+    print(y)
+
+print("-"*80)
+
+'''range com 3 parametros
+[1º paramtro, 2ºparametro[
+3º parametro = passo
+'''
+
+for z in range(0,22,3):
+    print(z)
+
+print("-"*80)
+
+#range com passo negativo
+for k in range(10,0,-1):
+    print(k)
