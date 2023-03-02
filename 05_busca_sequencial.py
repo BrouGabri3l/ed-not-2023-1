8904393fb0349135d4f81342a327749a985fd91e
Gabriel Rodrigues Bolonha Santos <gabriel-rodriguesbs@hotmail.com>
2023-03-02 19:22:21 -0300

    feat: add tests to sequencial search file
    adding tests to sequencial search execution with search time

--- 05_busca_sequencial.py
+++ 05_busca_sequencial.py
@@ -13,11 +13,55 @@ def busca_sequencial(lista,val):
     #se percorrer toda a lista e nao encontrar retorna -1
     return -1
 ##########################
+# TESTES
 
-#TESTES
+# Procurando o valor 15
+resultado = busca_sequencial(nums, 15)
+print(f"Posição do valor 15 na lista: {resultado}")
 
-resultado = busca_sequencial(nomes_desord,"GABRIEL")
+# Procurando o valor 20
+resultado = busca_sequencial(nums, 20)
+print(f"Posição do valor 20 na lista: {resultado}")
 
-print(f"Posição do nome GABRIEL: {resultado}")
+# Procurando o valor 33
+resultado = busca_sequencial(nums, 33)
+print(f"Posição do valor 33 na lista: {resultado}")
+
+# TESTES COM NOMES
+
+import sys
+sys.dont_write_bytecode = True      # Impede a criação do cache
+
+from time import time
+
+from data.lista_nomes import nomes
+
+# Busca pelo nome GABRIEL
+hora_ini = time()
+resultado = busca_sequencial(nomes, "GABRIEL")
+hora_fim = time()
+print(f"Posição do nome GABRIEL na lista: {resultado}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms")
+
+# Busca pelo nome CARLOS
+hora_ini = time()
+resultado = busca_sequencial(nomes, "CARLOS")
+hora_fim = time()
+print(f"Posição do nome CARLOS na lista: {resultado}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms")
+
+# Busca pelo nome YARA
+hora_ini = time()
+resultado = busca_sequencial(nomes, "YARA")
+hora_fim = time()
+print(f"Posição do nome YARA na lista: {resultado}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms")
+
+# Busca pelo nome ORKUTILSON
+hora_ini = time()
+resultado = busca_sequencial(nomes, "ORKUTILSON")
+hora_fim = time()
+print(f"Posição do nome ORKUTILSON na lista: {resultado}")
+print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms")
 
 
